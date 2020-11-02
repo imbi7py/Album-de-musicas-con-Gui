a7369a19a4c05f524806edddd87d3936423c437f
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-11-02 18:57:44 -0500

    se agrego la informacion de la creacion de la db

--- sqldbjk.sql
+++ sqldbjk.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE IF NOT EXISTS album;
+CREATE TABLE registro (
+   id_registro int(11) NOT NULL AUTO_INCREMENT,
+   nombre_musica varchar(50) NOT NULL,
+   autor varchar(60) DEFAULT NULL,
+   genero varchar(30) DEFAULT NULL,
+   año varchar(10) DEFAULT NULL,
+   duracion varchar(10) DEFAULT NULL,
+  PRIMARY KEY (id_registro)
+);
 SELECT * FROM album.registro;
